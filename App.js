3faf6a142bf3f2d08e110779ffdad515a82cff7e
m-ssilva <bleed-it-out@live.com>
2019-05-03 13:49:23 -0300

    refactor(src): general refactor, components folder and structure

--- App.js
+++ App.js
@@ -1,78 +1,13 @@
 import React, { Component } from 'react'
-import { Container, Header, Content, Form, Item, Input, Label, Button, Text, Toast, Root } from 'native-base'
-import * as Expo from 'expo'
+import { AppRegistry, StyleSheet, Text, View } from 'react-native'
+import Login from './components/Login'
 
-export default class FloatingLabelExample extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      loading: true,
-      emailInput: '',
-      passwordInput: '',
-      showToast: false
-    }
-  }
-  async componentDidMount() {
-    await Expo.Font.loadAsync({
-      Roboto: require("native-base/Fonts/Roboto.ttf"),
-      Roboto_medium: require("native-base/Fonts/Roboto_medium.ttf"),
-      Ionicons: require("@expo/vector-icons/fonts/Ionicons.ttf"),
-    })
-    this.setState({ loading: false })
-  }
-  checkInput = () => {
-    if (this.state.emailInput != '') {
-      if (this.state.passwordInput != '') {
-        Toast.show({
-          text: 'Sucesso',
-          buttonText: 'OK',
-          duration: 3000
-        })
-      } else {
-        Toast.show({
-          text: 'Informe uma senha',
-          buttonText: 'OK',
-          duration: 3000
-        })
-      }
-    } else {
-      Toast.show({
-        text: 'Informe um e-mail',
-        buttonText: 'OK',
-        duration: 3000
-      })
-    }
-  }
+export default class Index extends Component {
   render() {
-    if (this.state.loading) {
-      return <Expo.AppLoading />
-    }
     return (
-      <Root>
-        <Container>
-          <Header />
-          <Content>
-            <Form>
-              <Item floatingLabel>
-                <Label>Email</Label>
-                <Input
-                  onChangeText={emailInput => this.setState({ emailInput })} />
-              </Item>
-              <Item floatingLabel last>
-                <Label>Password</Label>
-                <Input
-                  secureTextEntry={true}
-                  onChange={passwordInput => this.setState({ passwordInput })} />
-              </Item>
-            </Form>
-            <Button
-              block success
-              onPress={this.checkInput}>
-              <Text>Success</Text>
-            </Button>
-          </Content>
-        </Container >
-      </Root>
-    );
+      <Login />
+    )
   }
 }
+
+AppRegistry.registerComponent('Index', () => Index)
---
+++ components/Login.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react'
+import { View, Text, Image, StyleSheet, KeyboardAvoidingView } from 'react-native';
+import LoginForm from './LoginForm';
+
+export default class Login extends Component {
+  render() {
+    return (
+      <KeyboardAvoidingView behavior="padding" style={styles.container}>
+        <View style={styles.loginContainer}>
+          <Image resizeMode="contain" style={styles.logo} source={require('../assets/logo-balmung.png')} />
+        </View>
+        <View style={styles.formContainer}>
+          <LoginForm />
+        </View>
+      </KeyboardAvoidingView>
+    )
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#2c3e50'
+  },
+  loginContainer: {
+    alignItems: 'center',
+    flexGrow: 1,
+    justifyContent: 'center'
+  },
+  logo: {
+    position: 'absolute',
+    width: 496,
+    height: 144
+  },
+  title: {
+    color: '#FFF',
+    marginTop: 120,
+    width: 100,
+    textAlign: 'center',
+    opacity: 0.9
+  }
+})
